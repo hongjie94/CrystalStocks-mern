76ba272ec58641a2c7f5c415da8c86c76df2b16f
HONG JIE ZHANG <70243495+hongjie94@users.noreply.github.com>
2021-07-17 04:05:11 -0400

    Update UserContext.js

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -7,6 +7,16 @@ export const TransactionContext = createContext({});
 
 export default function Context({children}) {
     
+  let headers = new Headers();
+
+  headers.append('Content-Type', 'application/json');
+  headers.append('Accept', 'application/json');
+
+  headers.append('Access-Control-Allow-Origin', 'https://crystalstocks.netlify.app');
+  headers.append('Access-Control-Allow-Credentials', 'true');
+
+  headers.append('GET', 'POST', 'OPTIONS');  
+    
   const [UserObject, setUserObject] = useState('');
   const [StockHistories, setStockHistories] = useState('');
   const [UserHoldings, setUserHoldings] = useState('');
@@ -333,10 +343,7 @@ export default function Context({children}) {
 
 
   useEffect(() => {
-    const ac = new AbortController();
     getUserObjects();
-    return () => ac.abort();
-
   },[getUserObjects, UpdateUserObject]);
 
   return (
