77510ec491f85242b1c0ea7b9a3186d5c5d79407
Zack <Home@MacBook-Air.local>
2021-07-20 18:39:42 -0400

    local

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -70,8 +70,7 @@ export default function Context({children}) {
   // Get User Objects
   const getUserObjects =  useCallback(async () =>{
     await axios.get( `${baseURL}/auth/getuser`, { 
-      withCredentials: true,
-      headers: {'Access-Control-Allow-Origin': 'https://crystalstocks.netlify.app', 'Content-Type': 'application/json'}
+      withCredentials: true
     }).then((res) => {
         if (res.data) {
           setUserObject(res.data);
