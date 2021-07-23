d445a46c5c22feac2ce9bfc2612687627790a97a
Zack <Home@MacBook-Air.local>
2021-07-22 20:07:21 -0400

    update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -50,7 +50,8 @@ export const Navbar = (LoginObject) => {
     }).then((res) => {
       if(res.data === 'Successfully Authenticated') {
         toast.success("Hello. You are now successfully logged in. Welcome back!");
-        window.location.href = '/history';
+        history.push('/history');
+        LoginObject.getUserObjects();
       }
     });
   };
